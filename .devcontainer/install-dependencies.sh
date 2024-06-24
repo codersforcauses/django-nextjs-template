1cbe05c0f0925134d3724255d87b126299446dbe
Dylan To <dylan.to@hotmail.com>
2024-06-24 16:23:02 +0000

    Dev container setup script

--- .devcontainer/install-dependencies.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-cd server && POETRY_VIRTUALENVS_CREATE=false poetry install # give editor intellisense
---
+++ .devcontainer/setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Runs once after devcontainer is created
+
+figlet "1-time Setup"
+echo "Running one-time setup for you :)"
+
+# Create .env files if it doesn't exist
+if [ ! -f ./client/.env ]; then
+    cp ./client/.env.example ./client/.env
+fi
+if [ ! -f ./server/.env ]; then
+    cp ./server/.env.example ./server/.env
+fi
+
+# Give python editor intellisense
+cd server && POETRY_VIRTUALENVS_CREATE=false poetry install 
+
+# Build containers so less waiting
+docker compose pull
