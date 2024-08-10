94d5fdc4efe67130a4ba4039498290d79a92773a
Dylan To <dylan.to@hotmail.com>
2024-08-10 13:04:08 +0000

    Avoid static warnings in setup

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -13,12 +13,12 @@ if [ ! -f ./server/.env ]; then
     cp ./server/.env.example ./server/.env
 fi
 
+# Make the Django static folder to remove the annoying warning
+(cd server && mkdir -p static)
+
 # Install dependencies
 (cd server && POETRY_VIRTUALENVS_CREATE=false poetry install)
 (cd client && npm install)
 
 # Nuke and migrate db
 (cd server && ./nuke.sh)
-
-# Make the Django static folder to remove the annoying warning
-(cd server && mkdir -p static)
