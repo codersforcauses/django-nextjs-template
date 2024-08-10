9ed2fb180b72c6ba73e9e0fe6375e2cc884a5d1d
Dylan To <dylan.to@hotmail.com>
2024-08-10 13:00:41 +0000

    Revert "Fix .env file copying on fresh clone"
    This reverts commit b3aa1b83bbf651d026cd01219093eccb5c9aa4ab.

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -5,6 +5,14 @@
 figlet "1-time Setup"
 echo "Running one-time setup for you :)"
 
+# Create .env files if it doesn't exist
+if [ ! -f ./client/.env ]; then
+    cp ./client/.env.example ./client/.env
+fi
+if [ ! -f ./server/.env ]; then
+    cp ./server/.env.example ./server/.env
+fi
+
 # Install dependencies
 (cd server && POETRY_VIRTUALENVS_CREATE=false poetry install)
 (cd client && npm install)
