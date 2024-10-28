d7ad8979eac0dfaab5aa1e45df0abd36969c914a
Frinze Erin Lapuz <frinzekt@gmail.com>
2024-10-28 20:39:00 +0800

    Fix review comments

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,7 +6,7 @@ figlet "1-time Setup"
 echo "Running one-time setup for you :)"
 
 # We can set up in global because this only applies to the entire devcontainer
-git config --global --add safe.directory /workspace
+git config --global --add safe.directory .
 git config --global --add --bool push.autoSetupRemote true
 
 # Create .env files if it doesn't exist
