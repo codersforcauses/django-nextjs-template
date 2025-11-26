8e2927878da820ad287cce0fd59eb7e0d2b416fe
Ben Fitzhardinge <ben@fitzhardinge.net>
2025-11-26 13:09:17 +0800

    updated server Docker to use uv too

--- docker/server/entrypoint.sh
+++ docker/server/entrypoint.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 echo "Applying database migrations"
-python manage.py migrate --noinput
+uv run python manage.py migrate --noinput
 
 echo "Collecting static files"
-python manage.py collectstatic --noinput
+uv run python manage.py collectstatic --noinput
 
 # Create Django Superuser
 echo "Creating Django Superuser"
-python manage.py createsuperuser --noinput
+uv run python manage.py createsuperuser --noinput
 
 # Run inbuilt Django server if ENV is development
 if [ "${APP_ENV^^}" = "DEVELOPMENT" ]; then
 
     # Install extra non-prod packages
     printf "\n" && echo "Installing dev dependencies for $APP_ENV"
-    uv sync 
+    uv sync --frozen --all-groups
     # Run developments
     printf "\n" && echo "Starting inbuilt django webserver"
     echo "Running: python manage.py runserver 0.0.0.0:8081"
-    python manage.py runserver 0.0.0.0:8081
+    uv run python manage.py runserver 0.0.0.0:8081
     exit
 fi
 
@@ -31,5 +31,5 @@ if [ "${APP_ENV^^}" = "PRODUCTION" ]; then
     # Run Gunicorn / Django
     printf "\n" && echo " Running Gunicorn / Django"
     echo "Running: gunicorn api.wsgi -b 0.0.0.0:8081 --workers=6 --keep-alive 20 --log-file=- --log-level debug --access-logfile=/var/log/accesslogs/gunicorn --capture-output --timeout 50"
-    gunicorn api.wsgi -b 0.0.0.0:8081 --workers=6 --keep-alive 20 --log-file=- --log-level debug --access-logfile=/var/log/accesslogs/gunicorn --capture-output --timeout 50
+    uv run gunicorn api.wsgi -b 0.0.0.0:8081 --workers=6 --keep-alive 20 --log-file=- --log-level debug --access-logfile=/var/log/accesslogs/gunicorn --capture-output --timeout 50
 fi
--- server/api/settings.py
+++ server/api/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "django_extensions",
+    # "django_extensions",
     "rest_framework",
     "corsheaders",
     "api.healthcheck",
