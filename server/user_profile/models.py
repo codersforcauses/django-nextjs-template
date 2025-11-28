57cdfdf94db2f9c3f651aa142412646d34809045
Ben Fitzhardinge <ben@fitzhardinge.net>
2025-11-28 16:40:50 +0800

    add signal for userprofile

--- server/user_profile/models.py
+++ server/user_profile/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
 
 class Profile(models.Model):
     user = models.OneToOneField("auth.User", on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
+
+@receiver(post_save, sender="auth.User")  # new
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    Profile.objects.get_or_create(user=instance)
