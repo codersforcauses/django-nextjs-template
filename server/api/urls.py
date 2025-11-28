a6150d3c397a81874c49e11bead648fd83f03bc2
Ben Fitzhardinge <ben@fitzhardinge.net>
2025-11-28 16:12:59 +0800

    move server/api/healthcheck to server/healthcheck

--- server/api/urls.py
+++ server/api/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/healthcheck/", include(("api.healthcheck.urls"))),
+    path("api/healthcheck/", include(("healthcheck.urls"))),
 ]
